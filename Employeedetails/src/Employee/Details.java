1ab3f6cd6cb4e05ce47acecf74a66373cf44d8ed
mkbansing <81643833+mkbansing@users.noreply.github.com>
2021-04-05 20:21:15 +0530

    Update Details.java

--- Employeedetails/src/Employee/Details.java
+++ Employeedetails/src/Employee/Details.java
@@ -1,4 +1,4 @@
-package Employee;
+package employee;
 import java.util.Scanner;
 import java.util.ArrayList;
 public class Details {
@@ -7,56 +7,56 @@ public class Details {
 		ArrayList<Employee> empList = new ArrayList<>();
 		Scanner s = new Scanner(System.in);
 		char c ;
+		String ch;
 	
 	System.out.println("\n Welcome To Employee Registration System\n");
 	do
 	{
-		System.out.println("1. Press 1 to ADD Employee details");
-		System.out.println("2. Press 2 to UPDATE Employee details");
-		System.out.println("3. Press 3 to DELETE Employee details");
-		System.out.println("4. Press 4 to PRINT Employee details");
-		System.out.println("5. Press 5 to SEARCH Employee details by ID\n");
+		System.out.println("1. Press A to ADD Employee details");
+		System.out.println("2. Press U to UPDATE Employee details");
+		System.out.println("3. Press D to DELETE Employee details");
+		System.out.println("4. Press P to PRINT Employee details");
+		System.out.println("5. Press S to SEARCH Employee details by ID\n");
 		
 		System.out.print("Enter your choice : ");
-		int ch = s.nextInt();
-		s.nextLine();
+		ch = s.next();
 		System.out.println("\n");
 		
 		switch(ch)
 		{
-		case 1:
+		case "A":
 			empAdd(empList);
 			break;
 			
-		case 2:
+		case "U":
 			System.out.print("Enter the Employee Id : ");
-			long empIdUpdate = s.nextLong();
-			empDetailsUpdate(empList, empIdUpdate);
+			empIdU = s.nextLong();
+			empDetailsUpdate(empList, empIdU);
 			break;
 			
-		case 3:
+		case "D":
 			System.out.print("Enter the Employee Id : ");
-			long empIdDelete = s.nextLong();
-			empDelete(empList, empIdDelete);
+			empIdD = s.nextLong();
+			empDelete(empList, empIdD);
 			break;
 			
-		case 4:
+		case "P":
 			empGet(empList);
 			break;
 			
-		case 5:
+		case "S":
 			System.out.print("Enter the Employee Id : ");
-			long empIdSearch = s.nextLong();
-			empGet(empList, empIdSearch);
+			empIdSe = s.nextLong();
+			empGet(empList, empIdSe);
 			break;
 			
 		default:
 			System.out.println("Invalid Choice");
 		}
-		System.out.print("Do you want to continue with the Registration System(Press Y for Yes or Press N for No) ? : ");
-		c = s.next().charAt(0);
+		System.out.print("Do you want to continue with the Registration System(Press Y/y for Yes or Press N/n for No) ? : ");
+		c = s.next();
 		
-	}while(c == 'Y');
+	}while(c=='Y'|| c=='y');
 	
 	s.close();
 	
@@ -66,7 +66,7 @@ public static void empAdd(ArrayList<Employee> empList)
 {
 	Scanner s = new Scanner(System.in);
 	
-	if(empList.size()==0)
+	if(empList.length()==0)
 	{
 		System.out.print("Enter Employee Name : ");
 		String empName = s.nextLine();
@@ -97,14 +97,101 @@ public static void empAdd(ArrayList<Employee> empList)
 		Employee emp = new Employee(empId+1, empName, empAge, empAddress);
 		empList.add(emp);
 	}	
-	//s.close();
+	s.close();
 		
+}
+public static void empDetailsUpdate(ArrayList<Employee> empList, long empId)
+{
+	int flag = 0;
+	char c;
+	Scanner s = new Scanner(System.in);
+	
+	for(int i=0;i<empList.length();i++)
+	{
+		if(empList.get(i).getEmpId() == empId)
+		{
+			do
+			{
+				System.out.println("Select the field you want to update for "+empId+" : \n");
+				System.out.println("Press 1 for Employee Name\n Press 2 for Employee Age\n Press 3 for Employee Address\n");
+				System.out.print("Enter your choice : ");
+				int ch = s.nextInt();
+				
+				switch(ch)
+				{
+				case 1:
+					System.out.print("Enter the updated Name field : ");
+					String name = s.nextLine();
+					empList.setEmpName(name);
+					System.out.println("Employee Name updated for "+empId+"\n");
+					flag = 1;
+					break;
+				case 2:
+					System.out.print("Enter the updated Age field : ");
+					int age = s.nextInt();
+					empList.setEmpAge(age);
+					System.out.println("Employee Age updated for "+empId+"\n");
+					flag = 1;
+					break;
+				case 3:
+					System.out.print("Enter the updated Address field : ");
+					String address = s.nextLine();
+					empList.setEmpAddress(address);
+					System.out.println("Employee Address updated for "+empId+"\n");
+					flag = 1;
+					break;
+				default:
+					System.out.println("Invalid choice\n");
+				}
+				System.out.print("Do you want to Update any field again for "+empId+"(Press Y for Yes or Press N for No)? : ");
+				c = s.next();
+				
+			}while(c == 'Y');
+			
+		}
+	}
+	s.close();
+	if(flag == 0)
+	{
+		System.out.println("No employee is present with the given employee Id.\n");
+	}
+}
+public static void empDelete(ArrayList<Employee> empList, long empId)
+{
+	Scanner s = new Scanner(System.in);
+	int flag = 0;
+	
+	for(int i = 0; i<empList.length();++i)
+	{
+		if(empList.get(i).getEmpId() == empId)
+		{
+			System.out.print("Do you want to delete the record for "+empId+"(Press Y for Yes or Press N for No)? : ");
+			char ch =  s.next().charAt(0);
+			if(ch == 'Y')
+			{
+				empList.delete(i);
+				System.out.println("Record deleted");
+				flag = 1;
+			}
+			else 
+			{
+				System.out.println("You chose not to delete the record for "+empId+"\n");
+				flag = 1;
+			}
+			break;
+		}
+	}
+	s.close();
+	if(flag == 0)
+	{
+		System.out.println("No employee is present with the given employee Id.\n");
+	}
 }
 
 public static void empGet(ArrayList<Employee> empList)
 {
 	System.out.println("Employee ID\t\tEmployee Name\t\tEmployee Age\t\tEmployee Address\n");
-	for(int i=0;i<empList.size();i++)
+	for(int i=0;i<empList.length();i++)
 	{
 		System.out.println(empList.get(i));
 	}
@@ -115,7 +202,7 @@ public static void empGet(ArrayList<Employee> empList, long empId)
 {
 	int flag = 0;
 	
-	for(int i=0; i<empList.size();i++)
+	for(int i=0; i<empList.length();i++)
 	{
 		if(empList.get(i).getEmpId() == empId)
 		{
@@ -131,96 +218,4 @@ public static void empGet(ArrayList<Employee> empList, long empId)
 		System.out.println("No employee is present with the given employee Id.\n");
 	}
 }
-
-public static void empDelete(ArrayList<Employee> empList, long empId)
-{
-	Scanner s = new Scanner(System.in);
-	int flag = 0;
-	
-	for(int i = 0; i<empList.size();++i)
-	{
-		if(empList.get(i).getEmpId() == empId)
-		{
-			System.out.print("Do you want to delete the record for "+empId+"(Press Y for Yes or Press N for No)? : ");
-			char ch =  s.next().charAt(0);
-			if(ch == 'Y')
-			{
-				empList.remove(i);
-				System.out.println("Record deleted");
-				flag = 1;
-			}
-			else 
-			{
-				System.out.println("You chose not to delete the record for "+empId+"\n");
-				flag = 1;
-			}
-			break;
-		}
-	}
-	//s.close();
-	if(flag == 0)
-	{
-		System.out.println("No employee is present with the given employee Id.\n");
-	}
 }
-
-public static void empDetailsUpdate(ArrayList<Employee> empList, long empId)
-{
-	int flag = 0;
-	char c;
-	Scanner s = new Scanner(System.in);
-	
-	for(int i=0;i<empList.size();i++)
-	{
-		if(empList.get(i).getEmpId() == empId)
-		{
-			do
-			{
-				System.out.println("Select the field you want to update for "+empId+" : \n");
-				System.out.println("Press 1 for Employee Name\n Press 2 for Employee Age\n Press 3 for Employee Address\n");
-				System.out.print("Enter your choice : ");
-				int ch = s.nextInt();
-				s.nextLine();
-				
-				switch(ch)
-				{
-				case 1:
-					System.out.print("Enter the updated Name field : ");
-					String name = s.nextLine();
-					empList.get(i).setEmpName(name);
-					System.out.println("Employee Name updated for "+empId+"\n");
-					flag = 1;
-					break;
-				case 2:
-					System.out.print("Enter the updated Age field : ");
-					int age = s.nextInt();
-					s.nextLine();
-					empList.get(i).setEmpAge(age);
-					System.out.println("Employee Age updated for "+empId+"\n");
-					flag = 1;
-					break;
-				case 3:
-					System.out.print("Enter the updated Address field : ");
-					String address = s.nextLine();
-					empList.get(i).setEmpAddress(address);
-					System.out.println("Employee Address updated for "+empId+"\n");
-					flag = 1;
-					break;
-				default:
-					System.out.println("Invalid choice\n");
-				}
-				System.out.print("Do you want to Update any field again for "+empId+"(Press Y for Yes or Press N for No)? : ");
-				c = s.next().charAt(0);
-				
-			}while(c == 'Y');
-			
-		}
-	}
-	//s.close();
-	if(flag == 0)
-	{
-		System.out.println("No employee is present with the given employee Id.\n");
-	}
-}
-}
-
